be1ca4f849d7812aac1fc04a779013e3189283c2
VidraSpy <terminatorroland@gmail.com>
2018-10-01 12:55:15 +0200

    start

---
+++ BejegyzesProjekt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+
+namespace BejegyzesProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
